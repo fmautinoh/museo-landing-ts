1a70312f06260220af01823aac28ba4351fa68d7
fmautinoh20 <fmautinoh20@gmail.com>
2024-07-31 04:59:43 -0500

    import

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,8 +1,4 @@
 import { Landing } from '@/components/component/landing';
-<<<<<<< HEAD
-import { useTranslations, } from 'next-intl';
-=======
->>>>>>> Internationalization-Module
 
 export default function HomePage() {
     
